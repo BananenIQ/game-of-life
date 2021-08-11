bbb08e14659ed692aaff44d3f54248afcf0deb5b
BananenIQ <krystianprzywozny@gmail.com>
2021-08-11 23:39:37 +0200

    added a Time-Machine and a running simulation - made some refactorings

--- GoL.py
+++ GoL.py
@@ -1,19 +1,23 @@
 import pygame as game
 import numpy as np
-import pygame_gui as gui
+import time
+
+# import pygame_gui as gui
 
 RUNNING = True
-SIZE_GRID = 75, 75
-WIDTH_GRID, HEIGHT_GRID = 20, 20
+SIZE_GRID = 200, 200
+WIDTH_GRID, HEIGHT_GRID = 7, 7
 MARGIN = 1
+TIMER = 50
 
 generation_dict = {}
 
 
-def run_GameUI(size_grid, w_grid, h_grid, margin, run, grid_array):
+def run_GameUI(size_grid, w_grid, h_grid, margin, run, map_array):
     window_size = size_grid[0] * (w_grid + margin), size_grid[1] * (h_grid + margin)
     game.init()
-    width, height = window_size
+    # width, height = window_size
+    sim_run = False
     generation = 0
 
     game.display.set_caption("Game of Life")
@@ -21,31 +25,43 @@ def run_GameUI(size_grid, w_grid, h_grid, margin, run, grid_array):
 
     while run:
 
+        tick = game.time.get_ticks()
+
         for event in game.event.get():
 
             if event.type == game.MOUSEBUTTONDOWN:
                 mouse_state = game.mouse.get_pressed(3)
                 if mouse_state[0]:
-                    grid_array = get_mouse_in_array(grid_array, w_grid, h_grid, margin)
+                    map_array = get_mouse_in_array(map_array, w_grid, h_grid, margin)
 
             if event.type == game.KEYDOWN:
                 if event.key == game.K_SPACE:
-                    grid_array = newGen(grid_array, generation)
+                    map_array = newGen(map_array, generation)
                     generation += 1
+                    print_generations(generation)
 
-                    print('Current Generation: {}'.format(generation))
                 if event.key == game.K_LEFT:
                     if generation > 0:
-                        grid_array = generation_dict.get(generation - 1)
+                        map_array = generation_dict.get(generation - 1)
                         generation -= 1
-                        print('Current Generation: {}'.format(generation))
+                        print_generations(generation)
 
                 if event.key == game.K_f:
-                    grid_array = get_mouse_in_array(grid_array, w_grid, h_grid, margin)
+                    map_array = get_mouse_in_array(map_array, w_grid, h_grid, margin)
+
+                if event.key == game.K_s:
+                    sim_run = not sim_run
+                    game.time.set_timer(game.NUMEVENTS-5, TIMER)
 
                 if event.key == game.K_r:
-                    grid_array.fill(0)
+                    map_array.fill(0)
+                    generation_dict.clear()
                     generation = 0
+
+                if event.key == game.K_t:
+                    map_array, generation = \
+                        time_machine(map_array, generation, int(input("Time Machine : Type a generation\n")))
+                    print_generations(generation)
 
                 if event.key == game.K_ESCAPE:
                     run = False
@@ -53,27 +69,55 @@ def run_GameUI(size_grid, w_grid, h_grid, margin, run, grid_array):
             if event.type == game.QUIT:
                 run = False
 
+            if sim_run:
+                if event.type == game.NUMEVENTS-5:
+                    map_array, generation = run_simulation(map_array, generation)
+                    print_generations(generation)
+                    game.time.set_timer(game.NUMEVENTS-5, TIMER)
+
         screen.fill("black")
 
-        for row in range(size_grid[0]):
-            for col in range(size_grid[1]):
-                if grid_array[row][col] == 0:
-                    game.draw.rect(screen, 'white',
-                                   [(margin + w_grid) * col + margin, (margin + h_grid) * row + margin, w_grid, h_grid])
-                else:
-                    game.draw.rect(screen, 'black',
-                                   [(margin + w_grid) * col + margin, (margin + h_grid) * row + margin, w_grid, h_grid])
+        draw_new_map(map_array, screen, size_grid, w_grid, h_grid, margin)
 
         game.display.update()
 
 
-def get_mouse_in_array(array_map, w_grid, h_grid, margin):
+def draw_new_map(map_array, screen, size_grid, w_grid, h_grid, margin):
+    for row in range(size_grid[0]):
+        for col in range(size_grid[1]):
+            if map_array[row][col] == 0:
+                game.draw.rect(screen, 'white',
+                               [(margin + w_grid) * col + margin, (margin + h_grid) * row + margin, w_grid, h_grid])
+            else:
+                game.draw.rect(screen, 'black',
+                               [(margin + w_grid) * col + margin, (margin + h_grid) * row + margin, w_grid, h_grid])
+
+
+def print_generations(generation):
+    print("Current Generations: {}".format(generation))
+
+
+def run_simulation(map_array, generation):
+    map_array = newGen(map_array, generation)
+    generation += 1
+    return map_array, generation
+
+
+def time_machine(map_array, generation_current, generation_limit):
+    generation = generation_current
+    for i in range(generation_limit):
+        map_array = newGen(map_array, generation)
+        generation += 1
+    return map_array, generation
+
+
+def get_mouse_in_array(map_array, w_grid, h_grid, margin):
     mouse_pos = game.mouse.get_pos()
-    if array_map[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] == 1:
-        array_map[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] = 0
+    if map_array[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] == 1:
+        map_array[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] = 0
     else:
-        array_map[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] = 1
-    return array_map
+        map_array[(mouse_pos[1] // (w_grid + margin))][(mouse_pos[0] // (h_grid + margin))] = 1
+    return map_array
 
 
 def newGen(current_map, generation: int):
@@ -113,14 +157,14 @@ def check_neighbor(current_map_copy, row, col):
 
 
 def main():
-    grid_array = np.zeros((SIZE_GRID[0], SIZE_GRID[1]), dtype=int)
+    map_array = np.zeros((SIZE_GRID[0], SIZE_GRID[1]), dtype=int)
 
     run_GameUI(size_grid=SIZE_GRID,
                w_grid=WIDTH_GRID,
                h_grid=HEIGHT_GRID,
                margin=MARGIN,
                run=RUNNING,
-               grid_array=grid_array)
+               map_array=map_array)
 
 
 if __name__ == '__main__':
